66200eb27c41e33e36068618db12c15990989591
Affy <iamafzal09@gmail.com>
2020-10-24 23:35:43 +0530

    add introduction [age

---
+++ resources/views/frontend/cms/introduction.blade.php
@@ -0,0 +1,36 @@
+@extends('frontend.layouts.app')
+@section('content')
+<!--How it works start-->
+<div class="row" style="margin-top:5%">
+    <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="lms_title_center">
+            <div class="lms_heading_1">
+                <h2 class="lms_heading_title">Introduction</h2>
+            </div>
+            <!-- <p>Data provided by the National Assessment of Educational Progress (NAEP)  and the Annie E. Casey Foundation showed that 80% of students from low-income families and 60% of all other students have such serious reading deficiencies they cannot read. As a result, it is extremely difficult to get students to use textbooks  for self-study or remedial purposes. The irony of this situation is that students who fail because they cannot read, are often placed in remedial programs where they are required to use the same textbooks that caused their previous failure.  As a result, teachers often must create and deliver homework assignments using paper based work sheets.  Although these work sheets can provide some support, they are not as effective as the repetitive practice and assessment that is provided by MathModus. </p> -->
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="lms_service_1">
+            <div class="lms_service_icon"><img src="{{ asset('frontend/images/services/services1.png')}}"
+                    alt="service icon" /></div>
+
+            <h3 class="lms_service_title">Introduction</h3>
+
+            <p>
+                MathModus is an online, easy-to-use blended learning system used in Mathematical subjects from grades 4 onwards. Through its use of computer based automation, MathModus delivers pre-developed instructional lessons, diagnostic assessment tests, and targeted remedial instruction. As a result, teachers will not be required to return home each evening to create lessons, tests, or to grade and diagnose student outcomes. Through MathModus’ use, teachers will always be aware of what is required of them to ensure subject matter mastery by their students. Simultaneously, students will have the capability to assume their own responsibility for subject matter mastery through independent self-paced study targeted to their needs by MathModus. Through automation, teachers can increase their diagnostic testing activities with the assurance that MathModus will not only diagnose a student’s problem, but will deliver a customized course of remedial study to each student based upon their test results. In addition,<div id="gotointroduction"></div> it also provides teachers with real-time diagnostic data that enables them to hold targeted remedial discussions with students in need of this critical assistance. Upon completion of these face-to-face discussions with their teacher, students can then use MathModus’ repetitive diagnostic assessment and remediation strategies as often as necessary to achieve mastery. Throughout this entire repetitive process, teachers are made aware of each student’s progress and use this real-time data to assist in the remedial process.
+                
+                <br>
+                <br>
+                <br>
+                Through its use of easy-to-use, intuitive commands, a teacher can orchestrate a classroom based instructional program where each student is working on his/her individual needs either with the teacher or during independent study. Students are provided with automated capabilities that will enable them to use Adaptive, Differentiated, or Prescriptive instructional methods customized by MathModus to their exact needs. Because of these self-directed learning strategies, teachers are freed to hold remedial discussions with other students. During these teacher led group discussions, teachers use the diagnostic data provided by MathModus to deliver instruction that is of importance to each student. When a student obtains the remedial instruction needed by them, they can go online to test themselves on the areas corrected by their teacher. At any time during the school year, students can review and retest themselves on any section of the curriculum in which they had problems. As multiple test types are always available to the students, they can retest on a single lesson, a unit of instruction, midterm or final exam as often as they feel necessary to prepare for EOY tests. MathModus “intelligent” oversight feature will continuously monitor their performance and alert them when it senses help is needed. Through its many features and functions, MathModus guarantees that every student will achieve success in math because its diagnostic capabilities identify and provide corrective instruction when it occurs. Because a student’s success is based on skills taught in earlier math subjects, MathModus delivers remediation for those foundation skills lessons they may have failed to master from prior year math subjects. During its use in the classroom, MathModus neither tells a teacher how nor what to teach. It simply supports every aspect of their instruction, assessment, diagnostics, remedial, and reporting needs that will make them far more effective, while substantially reducing their daily level of effort.
+            </p>
+        </div>
+    </div>
+</div>
+
+
+<!--How it works end-->
+@endsection
